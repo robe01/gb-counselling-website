625a28041dbb5bc24206be1a5f3f3956908d832c
robe01 <topdoghalox@hotmail.com>
2014-12-20 11:01:31 +0000

    Most up to date version yet

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+
+get_header(); ?>
+
+    <main class="container-fluid">
+        <section>
+            <h1>There Seems To Be An Error</h1>
+            <p>The page you are looking for has either been deleted, moved somewhere else or simply does not exist. Please go back to the main menu.</p>
+        </section>
+    </main>
+
+<?php
+get_footer();
